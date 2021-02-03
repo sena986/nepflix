7a4a5c4431d4cace7a62498f6a5222be71ce5288
sena986 <53561380+sena986@users.noreply.github.com>
2021-02-03 12:14:03 +0900

    css setup

---
+++ src/Components/Header.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Header = styled.header``;
+
+const List = styled.ul`
+  display: flex;
+  &:hover {
+    background-color: blue;
+  }
+`;
+
+const Item = styled.li``;
+
+const Slink = styled(Link)``;
+
+export default () => (
+  <Header>
+    <List>
+      <Item>
+        <Slink to="/">Movies</Slink>
+      </Item>
+      <Item>
+        <Slink to="/tv">TV</Slink>
+      </Item>
+      <Item>
+        <Slink to="/search">Search</Slink>
+      </Item>
+    </List>
+  </Header>
+);
