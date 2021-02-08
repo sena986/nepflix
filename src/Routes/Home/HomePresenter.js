2deeebbc38ae276beeb4a3979d0ba7588d15f3f1
sena986 <53561380+sena986@users.noreply.github.com>
2021-02-09 00:16:38 +0900

    Presenter & Poster ing

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,3 +1,51 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Section from "Components/Section";
+import Loader from "Components/Loader";
+import Message from "Components/Message";
+import Poster from "Components/Poster";
+
+const Container = styled.div`
+  padding: 0px 20px;
+`;
+
+const HomePresenter = ({ nowPlaying, upcoming, popular, loading, error }) =>
+  loading ? (
+    <Loader />
+  ) : (
+    <Container>
+      {nowPlaying && nowPlaying.length > 0 && (
+        <Section title="Now Playing">
+          {nowPlaying.map((movie) => (
+            <Poster />
+          ))}
+        </Section>
+      )}
+      {upcoming && upcoming.length > 0 && (
+        <Section title="Upcoming">
+          {upcoming.map((movie) => (
+            <Poster />
+          ))}
+        </Section>
+      )}
+      {popular && popular.length > 0 && (
+        <Section title="Popular">
+          {popular.map((movie) => (
+            <Poster />
+          ))}
+        </Section>
+      )}
+      {error && <Message color="#e74c3c" text={error} />}
+    </Container>
+  );
+
+HomePresenter.propTypes = {
+  nowPlaying: PropTypes.array,
+  upcoming: PropTypes.array,
+  popular: PropTypes.array,
+  loading: PropTypes.bool.isRequired,
+  error: PropTypes.string,
+};
+
+export default HomePresenter;
--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -11,11 +11,19 @@ export default class extends React.Component {
     error: null,
   };
 
-  handleSubmit = () => {
+  handleSubmit = (event) => {
+    event.preventDefault();
     const { searchTerm } = this.state;
     if (searchTerm !== "") {
       this.searchByTerm();
     }
+  };
+
+  updateTerm = (event) => {
+    const {
+      target: { value },
+    } = event;
+    this.setState({ searchTerm: value });
   };
 
   searchByTerm = async () => {
@@ -49,6 +57,7 @@ export default class extends React.Component {
         loading={loading}
         error={error}
         handleSubmit={this.handleSubmit}
+        updateTerm={this.updateTerm}
       />
     );
   }
